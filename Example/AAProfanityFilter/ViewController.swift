6565eaebd8930cf5a284425a09cc01f3dd3e64e9
M. Ahsan Ali <hafiz.m.ahsan.ali@gmail.com>
2019-09-16 12:31:21 +0500

    Update Demo

--- Example/AAProfanityFilter/ViewController.swift
+++ Example/AAProfanityFilter/ViewController.swift
@@ -13,19 +13,19 @@ import AAProfanityFilter
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
     }
 
-    override func viewDidAppear(_ animated: Bool) {
-
-        let ff: String = "Some String"
-        print(ff.aa_profanityFiltered())
-
-
+    @IBAction func action(_ sender: Any) {
+        label.text = textField.text?.aa_profanityFiltered()
     }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
